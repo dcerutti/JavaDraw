241c3f6c5f90eb5b69e299f7f1e4d5de1bcdda7c
Cory M <HiVoltList@gmail.com>
2011-02-16 16:47:40 -0500

    Added function to get process ID without hard-code

--- JDPAtest.java
+++ JDPAtest.java
@@ -6,6 +6,7 @@ import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 import java.io.File;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
 /**
  * The problem with the loading the agent part was that the port was originally set to 5000.
@@ -21,12 +22,13 @@ public class JDPAtest {
 
     public static void main(String[] args) throws AttachNotSupportedException, IOException, AgentLoadException, AgentInitializationException {
 
-        /* Right now the only way to get it to work is to manually enter the process ID (PID)
-         * Easiest way: run program in terminal after compiling (in terminal):
-         * $ java Interesting &
-         * The & makes it run in the background, and it spits out the PID
+        /* Testing a way to get a process ID on the fly with memory management
+         *  libraries. Seemed to work pretty well. pid() function does this
+         *  and is described within that function
          */
-        VirtualMachine vm = VirtualMachine.attach("7464");
+        String pid = pid(); //returns int (see function below)
+        System.out.println("The process ID would be: " + pid);
+        VirtualMachine vm = VirtualMachine.attach(pid);
 
         // get system properties in target VM
         java.util.Properties props = vm.getSystemProperties();
@@ -45,5 +47,18 @@ public class JDPAtest {
         vm.detach();
 
    }
+
+    public static String pid() {
+        String pid[] = new String[2];
+
+        //Memory management library that gets process id in the form of
+        //  JVMProcessNumber@machine, e.g., 8101@localhost
+        String hostName = ManagementFactory.getRuntimeMXBean().getName();
+
+        //split string into array so we can get only the process number needed
+        pid = hostName.split("@");
+
+        return pid[0];
+    }
 
 }

35fe6192e64a2a397de0457295ebecf790940d03
Steven Amy <steven.amy@edutekltd.com>
2011-02-17 11:25:19 -0500

    Virtual Machine connected to VMAcquirer

--- JDPAtest.java
+++ JDPAtest.java
@@ -1,11 +1,24 @@
 package jdpatest;
 	 		 	
+import com.sun.jdi.Field;
+import com.sun.jdi.ReferenceType;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.event.ClassPrepareEvent;
+import com.sun.jdi.event.EventQueue;
+import com.sun.jdi.event.EventSet;
+import com.sun.jdi.event.ModificationWatchpointEvent;
+import com.sun.jdi.event.VMDeathEvent;
+import com.sun.jdi.event.VMDisconnectEvent;
+import com.sun.jdi.request.ClassPrepareRequest;
+import com.sun.jdi.request.EventRequestManager;
+import com.sun.jdi.request.ModificationWatchpointRequest;
 import com.sun.tools.attach.AgentInitializationException;
 import com.sun.tools.attach.AgentLoadException;	 	
-import com.sun.tools.attach.AttachNotSupportedException;	 
-import com.sun.tools.attach.VirtualMachine;	 	
-import java.io.File;	 	
-import java.io.IOException;	 	
+import com.sun.tools.attach.AttachNotSupportedException;
+import java.io.IOException;
+
+
+
 
 	 	
 
@@ -32,46 +45,66 @@ public class JDPAtest {
      * http://download.oracle.com/javase/6/docs/jdk/api/attach/spec/com/sun/tools/attach/VirtualMachine.html
 	 	
      */
+  public static final String CLASS_NAME = "Interesting";
+  public static final String FIELD_NAME = "head";
 	 	
-    public static void main(String[] args) throws AttachNotSupportedException, IOException, AgentLoadException, AgentInitializationException
+    public static void main(String[] args) throws AttachNotSupportedException, IOException, AgentLoadException, AgentInitializationException, InterruptedException
 	 	
     {
 	 	
          // attach to target VM
 	 	
    //port 2281 was the PID used in my TicTacToe Program and it worked.
-	 	
-        VirtualMachine vm = VirtualMachine.attach("2881");
-	 	
 
-	 	
-        // get system properties in target VM
-	 	
-        java.util.Properties props = vm.getSystemProperties();
-	 	
+   VirtualMachine vm = new VMAcquirer().connect(8000);
 
-	 	
-        // construct path to management agent
-	 	
-        String home = props.getProperty("java.home");
-	 	
-        String agent = home + File.separator + "lib" + File.separator
-	 	
-            + "management-agent.jar";
-	 	
 
-	         // load agent into target VM
-	 	
-        vm.loadAgent(agent, "com.sun.management.jmxremote");
-	 	
 
-	 	
-        // detach
-	 	
-        vm.detach();
-	 	
+    // process events
 
+    EventQueue eventQueue = vm.eventQueue();
+    while (true) {
+      EventSet eventSet = eventQueue.remove();
+      for ( com.sun.jdi.event.Event event : eventSet) {
+        if (event instanceof VMDeathEvent
+            || event instanceof VMDisconnectEvent) {
+          // exit
+          return;
+        } else if (event instanceof ClassPrepareEvent) {
+          // watch field on loaded class
+          ClassPrepareEvent classPrepEvent = (ClassPrepareEvent) event;
+          ReferenceType refType = classPrepEvent
+              .referenceType();
+          addFieldWatch((VirtualMachine) vm,refType);
+        } else if (event instanceof ModificationWatchpointEvent) {
+          // a Test.foo has changed
+          ModificationWatchpointEvent modEvent = (ModificationWatchpointEvent) event;
+          System.out.println("old="
+              + modEvent.valueCurrent());
+          System.out.println("new=" + modEvent.valueToBe());
+          System.out.println();
+        }
+      }
+      eventSet.resume();
+    }
+    }
 	 	
-   }
-	 	
+  private static void addClassWatch(VirtualMachine vm) {
+    EventRequestManager erm = vm.eventRequestManager();
+    ClassPrepareRequest classPrepareRequest = erm
+        .createClassPrepareRequest();
+    classPrepareRequest.addClassFilter(CLASS_NAME);
+    classPrepareRequest.setEnabled(true);
+  }
+
+ private static void addFieldWatch(VirtualMachine vm,
+      ReferenceType refType) {
+    EventRequestManager erm = vm.eventRequestManager();
+    Field field = refType.fieldByName(FIELD_NAME);
+    ModificationWatchpointRequest modificationWatchpointRequest = erm
+        .createModificationWatchpointRequest(field);
+    modificationWatchpointRequest.setEnabled(true);
+  }
+
 }
+	 	

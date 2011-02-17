59aca73408ff55d55c89fb794d7b0ebe678d1562
Cory M <HiVoltList@gmail.com>
2011-02-17 11:40:41 -0500

    Changed and cleaned files from import

--- JDPAtest.java
+++ JDPAtest.java
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-package jdpatest;
-	 		 	
+
+package testconnect;
+
 import com.sun.jdi.Field;
 import com.sun.jdi.ReferenceType;
 import com.sun.jdi.VirtualMachine;
@@ -13,45 +13,31 @@ import com.sun.jdi.event.VMDisconnectEvent;
 import com.sun.jdi.request.ClassPrepareRequest;
 import com.sun.jdi.request.EventRequestManager;
 import com.sun.jdi.request.ModificationWatchpointRequest;
-import com.sun.tools.attach.AgentInitializationException;
-import com.sun.tools.attach.AgentLoadException;	 	
-import com.sun.tools.attach.AttachNotSupportedException;
-import java.io.IOException;
+//import testconnect.VMAcquirer;
 
-
-
-=======
 import com.sun.tools.attach.AgentInitializationException;
 import com.sun.tools.attach.AgentLoadException;
 import com.sun.tools.attach.AttachNotSupportedException;
-import com.sun.tools.attach.VirtualMachine;
-import java.io.File;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
-import java.util.*;
->>>>>>> a79b23ab7e8e927a3ac90efaf5d6851d9e1eabb8
-
 /**
  * The problem with the loading the agent part was that the port was originally set to 5000.
  * However, we don't know if that is the correct port so taking it out makes it find one that is
  * open and sets it itself. Now, when we build it, it will be successful.
  */
-	 	
-public class JDPAtest {
 
+public class JDPAtest {
     /**
      * http://download.oracle.com/javase/6/docs/jdk/api/attach/spec/com/sun/tools/attach/VirtualMachine.html
      */
-<<<<<<< HEAD
   public static final String CLASS_NAME = "Interesting";
   public static final String FIELD_NAME = "head";
-	 	
+
     public static void main(String[] args) throws AttachNotSupportedException, IOException, AgentLoadException, AgentInitializationException, InterruptedException
-	 	
+
     {
-	 	
+
          // attach to target VM
-	 	
+
    //port 2281 was the PID used in my TicTacToe Program and it worked.
 
    VirtualMachine vm = new VMAcquirer().connect(8000);
@@ -86,7 +72,7 @@ public class JDPAtest {
       eventSet.resume();
     }
     }
-	 	
+
   private static void addClassWatch(VirtualMachine vm) {
     EventRequestManager erm = vm.eventRequestManager();
     ClassPrepareRequest classPrepareRequest = erm
@@ -105,48 +91,3 @@ public class JDPAtest {
   }
 
 }
-	 	
-=======
-
-    public static void main(String[] args) throws AttachNotSupportedException, IOException, AgentLoadException, AgentInitializationException {
-
-        /* Testing a way to get a process ID on the fly with memory management
-         *  libraries. Seemed to work pretty well. pid() function does this
-         *  and is described within that function
-         */
-        String pid = pid(); //returns int (see function below)
-        System.out.println("The process ID would be: " + pid);
-        VirtualMachine vm = VirtualMachine.attach("1004");
-
-        // get system properties in target VM
-        Properties props = vm.getSystemProperties();
-
-        // construct path to management agent
-        String home = props.getProperty("java.home");
-
-        String agent = home + File.separator + "lib" + File.separator
-
-            + "management-agent.jar";
-
-	// load agent into target VM
-        vm.loadAgent(agent, "com.sun.management.jmxremote");
-
-        //detach
-        vm.detach();
-
-   }
-
-    public static String pid() {
-        String pid[] = new String[2];
-
-        //Memory management library that gets process id in the form of
-        //  JVMProcessNumber@machine, e.g., 8101@localhost
-        String hostName = ManagementFactory.getRuntimeMXBean().getName();
-
-        //split string into array so we can get only the process number needed
-        pid = hostName.split("@");
-
-        return pid[0];
-    }
-}
->>>>>>> a79b23ab7e8e927a3ac90efaf5d6851d9e1eabb8
--- VMAcquirer.java
+++ VMAcquirer.java
@@ -1,9 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package jdpatest;
+package testconnect;
 
 import com.sun.jdi.Bootstrap;
 import com.sun.jdi.VirtualMachine;

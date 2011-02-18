f9e3fc710d735085a435e23ce14153cfdefaea62
Dan <dcerutti.student@manhattan.edu>
2011-02-17 21:51:09 -0800

    added more test cases

--- src/JavaDrawMain.java
+++ src/JavaDrawMain.java
@@ -24,12 +24,12 @@ public class JavaDrawMain {
 			/*
 			 * Graphics Team Code Here
 			 */
-			
+
 			// Builds a Frame with a Close 'X' Option
 			Frame myframe = new Frame("Draw File");
 			myframe.setSize(800, 600);
 			myframe.addWindowListener(new FrameCloser());
-			
+
 			// Makes a Canvas
 			DrawTree drawArea = new DrawTree();
 			myframe.add(drawArea, BorderLayout.CENTER);
@@ -37,27 +37,28 @@ public class JavaDrawMain {
 			// Makes that frame visible
 			myframe.pack();
 			myframe.setVisible(true);
-			drawArea.BuildGnList(50,50);
-			
-			//Make menubar
+			drawArea.BuildGnList(50, 50);
+
+			// Make menubar
 			MenuBar mb = new MenuBar();
 			myframe.setMenuBar(mb);
 			Menu m = new Menu("File");
-	    	mb.add(m);
-	    	MenuItem m2 = new MenuItem("Save Drawing as PNG");
-	    	m.add(m2);
-	    	m2.addActionListener(new PhotoSaver(myframe, drawArea));
+			mb.add(m);
+			MenuItem m2 = new MenuItem("Save Drawing as PNG");
+			m.add(m2);
+			m2.addActionListener(new PhotoSaver(myframe, drawArea));
 
-			while (true) {
-				drawArea.repaint(); // Draw it
-			}
+			// while (true) {
+			// drawArea.repaint(); // Draw it
+			// }
+			drawArea.repaint();
 
 		} else if (answer == 1) {
 			/*
 			 * Debug Team Code Here Use this as your main()
 			 */
 
-		}else{
+		} else {
 			Node head = BuildSampleTree();
 			PrintBadTree(head, 0); // Prints Sample Tree
 		}
@@ -76,6 +77,10 @@ public class JavaDrawMain {
 		Node funtion2 = new Node(NODETYPE.FUNCTION, "FUNCTION2");
 		Node object1 = new Node(NODETYPE.OBJECT, "Square");
 
+		Node testOBJ = new Node(NODETYPE.FUNCTION, "TESTOBJ");
+
+		main.children.add(testOBJ);
+		testOBJ.children.add(main);
 		main.children.add(funtion1);
 		main.children.add(funtion2);
 		main.children.add(object1);
@@ -84,14 +89,19 @@ public class JavaDrawMain {
 		Node object2a = new Node(NODETYPE.OBJECT, "Circle Link");
 		object2.children.add(object2a);
 		Node object2b = new Node(NODETYPE.OBJECT, "Circle Link 2");
-		//object2.children.add(object2b);
+
+		object2b.children.add(funtion2);
+
+		object2.children.add(object2b);
 		Node function3 = new Node(NODETYPE.FUNCTION, "Function3");
 		funtion1.children.add(object2);
 		funtion1.children.add(function3);
 		Node object3 = new Node(NODETYPE.OBJECT, "Triagle");
 		Node object3a = new Node(NODETYPE.OBJECT, "Triagle 2");
 		function3.children.add(object3);
-		//function3.children.add(object3a);
+		function3.children.add(object3a);
+
+		function3.children.add(object2b);
 
 		return head;
 	}
@@ -133,49 +143,55 @@ public class JavaDrawMain {
 	 * Button & Window Listeners
 	 */
 	static class FrameCloser extends WindowAdapter {
-		
+
 		public void windowClosing(WindowEvent e) // Closes window when called
 		{
 			System.out.println("Goodbye"); // Prints Goodbye to System
 			System.exit(0);
 		}
 	}
-	
+
 	/*
-	 * This function adds and ActionListener on the MenuBar to allow the 
-	 * image on the canvas to be saved as a PNG file.
+	 * This function adds and ActionListener on the MenuBar to allow the image
+	 * on the canvas to be saved as a PNG file.
 	 */
 	static class PhotoSaver implements ActionListener {
 		Frame parent;
 		DrawTree drawArea;
-		
-		public PhotoSaver(Frame parent, DrawTree drawArea)
-		{
+
+		public PhotoSaver(Frame parent, DrawTree drawArea) {
 			this.parent = parent;
 			this.drawArea = drawArea;
 		}
-		
+
 		public void actionPerformed(ActionEvent arg0) {
 			FileDialog f1 = new FileDialog(parent, "Save Image as PNG");
 			f1.setMode(FileDialog.SAVE);
 			f1.setVisible(true);
-			
+
 			String filename = f1.getFile();
 			String directory = f1.getDirectory();
-			
+
 			try {
-				BufferedImage bi = new BufferedImage(drawArea.getWidth(), drawArea.getHeight(), BufferedImage.TYPE_INT_RGB); //creates an image
-				Graphics g = bi.getGraphics(); //returns an instance of the Graphics class, allowing the program to draw to the image
+				BufferedImage bi = new BufferedImage(drawArea.getWidth(),
+						drawArea.getHeight(), BufferedImage.TYPE_INT_RGB); // creates
+																			// an
+																			// image
+				Graphics g = bi.getGraphics(); // returns an instance of the
+												// Graphics class, allowing the
+												// program to draw to the image
 				g.setColor(Color.WHITE);
 				g.fillRect(0, 0, drawArea.getWidth(), drawArea.getHeight());
 				g.setColor(Color.BLACK);
-				drawArea.paint(g); //paints the content of the canvas onto the image
+				drawArea.paint(g); // paints the content of the canvas onto the
+									// image
 				File opfile = new File(directory + filename);
-				ImageIO.write(bi, "png", opfile); //writes the drawing to a PNG file
+				ImageIO.write(bi, "png", opfile); // writes the drawing to a PNG
+													// file
 			} catch (IOException ioe) {
 				System.out.println("Could Not Read File");
-			} 
+			}
 		}
-		
+
 	}
 }

67b0785deae1390b8d88b0c74dbe8717b3bbe25a
Dan <dcerutti.student@manhattan.edu>
2011-02-17 21:50:20 -0800

    added Loopback class

--- src/GraphicNode.java
+++ src/GraphicNode.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -13,9 +12,9 @@ public abstract class GraphicNode {
 }
 
 class GraphicFunction extends GraphicNode {
-	
-	public int x,y;
-	String name;
+
+	public int x, y;
+	String name = "";
 	public Color c = Color.blue;
 
 	public GraphicFunction() {
@@ -33,9 +32,9 @@ class GraphicFunction extends GraphicNode {
 }
 
 class GraphicObject extends GraphicNode {
-	
-	public int x,y;
-	String name;
+
+	public int x, y;
+	String name = "";
 	public Color c = Color.red;
 
 	public GraphicObject() {
@@ -53,8 +52,8 @@ class GraphicObject extends GraphicNode {
 }
 
 class GraphicLine extends GraphicNode {
-	
-	public int x1,y1,x2,y2;
+
+	public int x1, y1, x2, y2;
 	public Color c = Color.red;
 
 	public GraphicLine() {
@@ -64,8 +63,35 @@ class GraphicLine extends GraphicNode {
 	public void draw(Graphics g) {
 
 		g.setColor(c);
-		g.drawLine(x1, y1 , x2, y2 );
+		g.drawLine(x1, y1, x2, y2);
 
+	}
+
+}
+
+class GraphicLoop extends GraphicNode {
+
+	int x1, x2, x3, x4, y1, y2, y3, y4;
+
+	public GraphicLoop() {
+	}
+
+	@Override
+	public void draw(Graphics g) {
+
+		// g.setColor(Color.GREEN);
+		g.drawLine(x1, y1, x2, y2);
+		g.drawLine(x2, y2, x3, y3);
+		g.drawLine(x3, y3, x4, y4);
+
+		// left arrow
+		g.drawLine(x4, y4, x4 + 8, y4 - 8);
+		g.drawLine(x4, y4, x4 + 8, y4 + 8);
+	}
+
+	public void update(int furthestLoopX) {
+		x2 = furthestLoopX;
+		x3 = furthestLoopX;
 	}
 
 }

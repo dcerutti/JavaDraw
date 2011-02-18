4091bf4dfa44231918d1c17f8828405ef777cecd
Dan <dcerutti.student@manhattan.edu>
2011-02-17 21:49:33 -0800

    Added dynamic loops (move based on furthest "east" node )

--- src/DrawTree.java
+++ src/DrawTree.java
@@ -26,27 +26,43 @@ public class DrawTree extends Canvas {
 	// Sample Tree
 	Node test = JavaDrawMain.BuildSampleTree();
 	Vector<GraphicNode> gnList = new Vector<GraphicNode>();
+	Vector<GraphicLoop> loopList = new Vector<GraphicLoop>();
+
 	int globalLevel = 1;
+	int furthestNodeX;
+	int furthestLoopX = -1;
 
 	/*
 	 * Builds sample list
 	 */
-	public void BuildGnList(int nodeWidth,int nodeHeight) {
-		
+	public void BuildGnList(int nodeWidth, int nodeHeight) {
+
 		traverse(JavaDrawMain.BuildSampleTree(), 50, 50, nodeWidth, nodeHeight);
 	}
 
 	/*
-	 *  Creates Graphics Nodes from node tree and sets proper x,y cords.
-	 * 
+	 * Creates Graphics Nodes from node tree and sets proper x,y cords.
 	 */
-	public void traverse(Node node, int x, int y,int nodeWidth,int nodeHeight) {
+	public void traverse(Node node, int x, int y, int nodeWidth, int nodeHeight) {
 
 		if (node.nodeType == NODETYPE.FUNCTION) {
 
 			GraphicFunction gF = new GraphicFunction();
 			gF.x = x;
 			gF.y = y;
+
+			if (x >= furthestNodeX) {
+				furthestNodeX = x;
+
+				int newX = furthestNodeX + (nodeWidth / 2) + 150;
+				for (GraphicLoop loop : loopList) {
+					loop.update(newX);
+					furthestLoopX = newX;
+					newX += 10;
+				}
+
+			}
+
 			gF.name = node.name;
 
 			gnList.add(gF);
@@ -62,6 +78,10 @@ public class DrawTree extends Canvas {
 
 		}
 
+		node.graphicX = x;
+		node.graphicY = y;
+		node.visited = true;
+
 		int newy = 0;
 		int newx = 0;
 
@@ -74,21 +94,65 @@ public class DrawTree extends Canvas {
 
 		for (Node child : node.children) {
 
-			globalLevel++;
+			if (child.visited) {
 
-			newx = x + nodeWidth;
-			newy = globalLevel * nodeHeight;
-			lineDown.y2 = newy;
-			
-			//Line from main branch to new node
-			GraphicLine lineAcross = new GraphicLine();
-			lineAcross.x1 = x;
-			lineAcross.y1 = newy;
-			lineAcross.x2 = newx - (nodeWidth/2);
-			lineAcross.y2 = newy;
-			gnList.add(lineAcross);
+				GraphicLoop gLoop = new GraphicLoop();
+				gLoop.x1 = x;
+				gLoop.y1 = y + (nodeHeight / 2);
 
-			traverse(child, newx, newy, nodeWidth, nodeHeight);
+				if (furthestLoopX == -1) {
+					gLoop.x2 = furthestNodeX + (nodeWidth / 2) + 150;
+					furthestLoopX = gLoop.x2;
+				} else {
+					gLoop.x2 = furthestLoopX + 10;
+					furthestLoopX = gLoop.x2;
+				}
+
+				gLoop.y2 = gLoop.y1;
+
+				gLoop.x3 = gLoop.x2;
+				gLoop.y3 = child.graphicY;
+
+				gLoop.x4 = child.graphicX + (nodeWidth / 2);
+				gLoop.y4 = child.graphicY;
+
+				gnList.add(gLoop);
+				loopList.add(gLoop);
+
+			} else {
+
+				globalLevel++;
+
+				newx = x + nodeWidth;
+				newy = (globalLevel * nodeHeight) + (globalLevel * 5);
+				lineDown.y2 = newy;
+
+				// Line from main branch to new node
+				GraphicLine lineAcross = new GraphicLine();
+				lineAcross.x1 = x;
+				lineAcross.y1 = newy;
+				lineAcross.x2 = newx - (nodeWidth / 2);
+				lineAcross.y2 = newy;
+				gnList.add(lineAcross);
+
+				GraphicLine ar1 = new GraphicLine();
+				ar1.x1 = lineAcross.x2;
+				ar1.y1 = lineAcross.y2;
+				ar1.x2 = ar1.x1 - 8;
+				ar1.y2 = ar1.y1 - 8;
+
+				GraphicLine ar2 = new GraphicLine();
+				ar2.x1 = lineAcross.x2;
+				ar2.y1 = lineAcross.y2;
+				ar2.x2 = ar2.x1 - 8;
+				ar2.y2 = ar2.y1 + 8;
+
+				gnList.add(ar1);
+				gnList.add(ar2);
+
+				traverse(child, newx, newy, nodeWidth, nodeHeight);
+
+			}
 
 		}
 

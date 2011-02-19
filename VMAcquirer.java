1f8e10d0be8a9cc279bd34d0e4f46da909f3343a
Cory M <HiVoltList@gmail.com>
2011-02-19 13:00:54 -0500

    Modified for more efficient execution and easier reading

--- VMAcquirer.java
+++ VMAcquirer.java
@@ -11,48 +11,41 @@ import java.util.Map;
 
 public class VMAcquirer {
 
-  /**
-   * Call this with the localhost port to connect to.
-   */
-  public VirtualMachine connect(int port)
-      throws IOException {
-    String strPort = Integer.toString(port);
-    AttachingConnector connector = getConnector();
-    try {
-      VirtualMachine vm = connect(connector, strPort);
-      return vm;
-    } catch (IllegalConnectorArgumentsException e) {
-      throw new IllegalStateException(e);
+    /**
+     * Call this with the localhost port to connect to.
+     */
+    public VirtualMachine connect(int port) throws IOException {
+
+        String strPort = Integer.toString(port);
+        AttachingConnector connector = getConnector();
+        try {
+            VirtualMachine vm = connect(connector, strPort);
+            return vm;
+        } catch (IllegalConnectorArgumentsException e) {
+            throw new IllegalStateException(e);
+        }
     }
-  }
 
-  private AttachingConnector getConnector() {
-    VirtualMachineManager vmManager = Bootstrap
-        .virtualMachineManager();
-    for (Connector connector : vmManager
-        .attachingConnectors()) {
-      System.out.println(connector.name());
-      if ("com.sun.jdi.SocketAttach".equals(connector
-          .name())) {
-        return (AttachingConnector) connector;
-      }
+    private AttachingConnector getConnector() {
+        VirtualMachineManager vmManager = Bootstrap.virtualMachineManager();
+        for (Connector connector : vmManager.attachingConnectors()) {
+            System.out.println(connector.name());
+            if ("com.sun.jdi.SocketAttach".equals(connector.name())) {
+                return (AttachingConnector) connector;
+            }
+        }
+        throw new IllegalStateException();
     }
-    throw new IllegalStateException();
-  }
 
-  private VirtualMachine connect(
-      AttachingConnector connector, String port)
-      throws IllegalConnectorArgumentsException,
-      IOException {
-    Map<String, Connector.Argument> args = connector
-        .defaultArguments();
-    Connector.Argument pidArgument = args.get("port");
-    if (pidArgument == null) {
-      throw new IllegalStateException();
+    private VirtualMachine connect(AttachingConnector connector, String port)
+      throws IllegalConnectorArgumentsException,IOException {
+        Map<String, Connector.Argument> args = connector.defaultArguments();
+        Connector.Argument pidArgument = args.get("port");
+        if (pidArgument == null) {
+            throw new IllegalStateException();
+        }
+        pidArgument.setValue(port);
+
+        return connector.attach(args);
     }
-    pidArgument.setValue(port);
-
-    return connector.attach(args);
-  }
-
 }

da0fde8fa9862a490b8f712d6269d9b7ba7e8a1f
michaelscheidegger <mscheidegger.student@manhattan.edu>
2011-03-03 08:51:33 -0800

    Edited src/JavaDrawMain.java via GitHub

--- src/JavaDrawMain.java
+++ src/JavaDrawMain.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;

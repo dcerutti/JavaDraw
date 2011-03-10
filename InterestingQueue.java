3b1919014b696ad2501d54251c11b7c98a300f1a
jacknight <jacknight@gmail.com>
2011-03-10 11:37:03 -0500

    added interestingqueue.java

---
+++ InterestingQueue.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+public class InterestingQueue {
+        InterestingQueue next = null;
+        Integer data;
+
+        public InterestingQueue(Integer data)
+        {
+                this.data = data;
+        }
+
+        public static void main(String args[]) throws IOException
+        {
+                InterestingQueue head, tail;
+                head = tail = new InterestingQueue(5);
+                
+                // enqueue the numbers 0 through 20
+                for (int i = 0; i < 20; i++) {
+                        tail.next = new InterestingQueue((i * 7) % 20);
+                        tail = tail.next;
+                }
+
+                // dequeue 3 times
+                System.out.println("Removing: " + head.data);
+                head = head.next;
+                System.out.println("Removing: " + head.data);
+                head = head.next;
+                System.out.println("Removing: " + head.data);
+                head = head.next;
+
+                // enqueue 100
+                tail = (tail.next = new InterestingQueue(100));
+
+                InterestingQueue curr = head;
+                while (curr != null) {
+                        System.out.print(curr.data + " -> ");
+                        curr = curr.next;
+                }
+                System.out.println("END");
+                System.in.read();
+        }
+}
